ebea3986144ad9779981a7b1347d4fc81071d72f
Eric Scott <escott@televet.com>
2023-04-08 20:26:33 -0500

    fixing js issues

---
+++ bin/devil-src/devil.ts
@@ -0,0 +1,54 @@
+import path from 'path';
+import { cwd } from 'process';
+
+import { asserts } from './assertions';
+import { cache } from './cache';
+import { checks } from './checks';
+import { file } from './filesystem';
+import { loggers } from './loggers';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chalk = require("chalk");
+// TODO: Inspect, trace, in, out, cache
+export const devil = {
+  chalk,
+  _rootDir: process.env.DEVIL_DIR || cwd(),
+  setRootDir: function (dir: string) {
+    // eslint-disable-next-line functional/immutable-data, functional/no-this-expression
+    this._rootDir = dir;
+  },
+
+  read: function (title: string) {
+    // eslint-disable-next-line functional/no-this-expression
+    const root = this._rootDir;
+    const fullPath = path.join(root, `${title}.devil.jsonc`);
+    return file.read(fullPath);
+  },
+
+  cache: function <T>(
+    title: string,
+    fnToCache: () => Promise<T>,
+    bustCache: boolean,
+    cacheOptions: {
+      readonly skipCache: boolean;
+    }
+  ) {
+    // eslint-disable-next-line functional/no-this-expression
+    const root = this._rootDir;
+    const fullPath = path.join(root, `${title}.devil.jsonc`);
+    cache(fullPath, fnToCache, bustCache, cacheOptions);
+  },
+
+  write: function (title: string, data: unknown) {
+    // eslint-disable-next-line functional/no-this-expression
+    const root = this._rootDir;
+    const fullPath = path.join(root, `${title}.jsonc`);
+    return file.write(fullPath, data);
+  },
+
+  // Loggers are some of the most common cases so we won't require the full path
+  ...loggers,
+  file,
+  check: checks,
+  asserts,
+};
---
+++ bin/devil-src/loggers/trace.ts
@@ -0,0 +1,40 @@
+/* eslint-disable functional/functional-parameters */
+/* eslint-disable functional/prefer-readonly-type */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+
+import { get as stackGet } from './stack-helper';
+
+import { log } from './index';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chalk = require('chalk');
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const shortTrace = (location: any) => `${location.getFileName()}`;
+
+// NOTE: We are using shortTrace because it gets the file name at least
+// but the line number is inaccurate because of the compilation and I don't know enough
+// to fix it and for me file helps a lot as an easy thing to jump to in vscode
+
+/**
+ * 👁 Trace *tries* 🤞 to figure out which file you are in at this time and logs it out
+ *
+ * I use it to easily find where I am in the code when I am debugging.
+ * @param first Can be treated like a label as a string, but not necessary
+ * @param data Any follow up objects that you want to be logged out
+ */
+export const trace = (first: unknown, ...data: unknown[]) => {
+  const location = stackGet()[1];
+  if (typeof first === 'string') {
+    // Treat it like a label and color it
+    const location = stackGet()[1];
+    log(
+      `${chalk.redBright(first)}`,
+      ...data,
+      `@ ${chalk.blue(shortTrace(location))}`
+    );
+  } else {
+    log(first, ...data, `@ ${chalk.blue(shortTrace(location))}`);
+  }
+};
